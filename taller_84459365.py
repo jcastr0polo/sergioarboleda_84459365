ae813470671d47792ad532a4d417bbff834a9f05
Jhonatan Castro <jhonatan.castro@usa.edu.co>
2026-03-06 07:34:49 -0500

    Tablas de Multplicar

--- taller_84459365.py
+++ taller_84459365.py
@@ -15,3 +15,19 @@ contador = 1
 while contador <= 5:
     print("Número: " + str(contador))
     contador = contador + 1
+
+for i in range(0, 12, 3):
+    print("Número For: " + str(i))
+
+encontrado = False
+numerobuscado = 11
+numeros = [1, 3, 5, 7, 9]   
+for numero in numeros:
+    if numero == numerobuscado:
+        encontrado = True
+        break
+print("Número", numerobuscado, "encontrado:", encontrado)
+
+for i in range(1,4):
+    for j in range(1,4):
+        print("i=" + str(i) + " j=" + str(j))
